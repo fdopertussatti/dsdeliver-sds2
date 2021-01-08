0eaf11a15209eea1b977507154a7d7f3d2575230
Fernando Pertussatti <fdopertussatti@gmail.com>
2021-01-08 01:43:44 -0500

    Add Routes

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -10,10 +10,10 @@ function Footer() {
                <a href="https://www.youtube.com/c/DevSuperior" target="_new">
                     <YoutubeIcon/>
                </a>
-               <a href="https://linkedin/school/devsuperior/" target="_new">
+               <a href="https://www.linkedin.com/school/devsuperior/" target="_new">
                     <LinkedinIcon/>
                </a>
-               <a href="https://www.instagram.com/evsuperior.ig/" target="_new">
+               <a href="https://www.instagram.com/devsuperior.ig/" target="_new">
                     <InstagramIcon/>
                </a>
            </div>
